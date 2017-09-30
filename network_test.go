2006afab3cc6884f3f2c1e1b4fa9f4a40c36af60
Kegan Dougal <kegsay@gmail.com>
2017-09-30 01:04:21 +0100

    Add more tests

--- network_test.go
+++ network_test.go
@@ -1,7 +1,6 @@
 package automata
 
 import (
-	"fmt"
 	"math"
 	"testing"
 )
@@ -42,6 +41,74 @@ func TestANDGate(t *testing.T) {
 	activateNetwork(t, &network, []float64{1, 1}, []float64{1})
 }
 
+func TestORGate(t *testing.T) {
+	// Make the network.
+	inputLayer := NewLayer(2)
+	outputLayer := NewLayer(1)
+	inputLayer.Project(&outputLayer, LayerTypeAuto)
+	network := Network{
+		Input:  &inputLayer,
+		Hidden: nil,
+		Output: &outputLayer,
+	}
+
+	// Train it.
+	trainer := Trainer{
+		Network:      &network,
+		MaxErrorRate: 0.001,
+		LearnRate:    0.1,
+		Iterations:   1000,
+		CostFunction: &MeanSquaredErrorCost{},
+	}
+	err := trainer.Train([]TrainSet{
+		{[]float64{0, 0}, []float64{0}},
+		{[]float64{0, 1}, []float64{1}},
+		{[]float64{1, 0}, []float64{1}},
+		{[]float64{1, 1}, []float64{1}},
+	})
+	if err != nil {
+		t.Fatalf("trainer.Train threw error: %s", err.Error())
+	}
+
+	// Test it.
+	activateNetwork(t, &network, []float64{0, 0}, []float64{0})
+	activateNetwork(t, &network, []float64{0, 1}, []float64{1})
+	activateNetwork(t, &network, []float64{1, 0}, []float64{1})
+	activateNetwork(t, &network, []float64{1, 1}, []float64{1})
+}
+
+func TestNOTGate(t *testing.T) {
+	// Make the network.
+	inputLayer := NewLayer(1)
+	outputLayer := NewLayer(1)
+	inputLayer.Project(&outputLayer, LayerTypeAuto)
+	network := Network{
+		Input:  &inputLayer,
+		Hidden: nil,
+		Output: &outputLayer,
+	}
+
+	// Train it.
+	trainer := Trainer{
+		Network:      &network,
+		MaxErrorRate: 0.001,
+		LearnRate:    0.1,
+		Iterations:   1000,
+		CostFunction: &MeanSquaredErrorCost{},
+	}
+	err := trainer.Train([]TrainSet{
+		{[]float64{0}, []float64{1}},
+		{[]float64{1}, []float64{0}},
+	})
+	if err != nil {
+		t.Fatalf("trainer.Train threw error: %s", err.Error())
+	}
+
+	// Test it.
+	activateNetwork(t, &network, []float64{0}, []float64{1})
+	activateNetwork(t, &network, []float64{1}, []float64{0})
+}
+
 func activateNetwork(t *testing.T, network *Network, input, desiredOutput []float64) {
 	output, err := network.Activate(input)
 	if err != nil {
@@ -50,8 +117,6 @@ func activateNetwork(t *testing.T, network *Network, input, desiredOutput []floa
 	for i, out := range output {
 		if round(out) != desiredOutput[i] {
 			t.Errorf("%v : want %f, got %f (%f)", input, desiredOutput[i], round(out), out)
-		} else {
-			fmt.Println(input, " got ", out)
 		}
 	}
 }
