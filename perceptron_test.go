73dbeb445175b9909839199b38d7e44aecc8c313
Kegan Dougal <kegsay@gmail.com>
2017-10-05 19:42:17 +0100

    Add unpredictable input perceptron test (sine wave)

--- perceptron_test.go
+++ perceptron_test.go
@@ -2,14 +2,15 @@ package automata_test
 
 import (
 	"github.com/kegsay/automata"
+	"math"
+	"math/rand"
 	"testing"
 )
 
-func TestPerceptron(t *testing.T) {
-	// Train an XOR
+func TestPerceptronXOR(t *testing.T) {
 	perceptron, err := automata.NewPerceptron([]int{2, 3, 1})
 	if err != nil {
-		t.Errorf("Failed to create NewPerceptron: %s", err.Error())
+		t.Fatalf("Failed to create NewPerceptron: %s", err.Error())
 	}
 
 	// Train it.
@@ -35,4 +36,51 @@ func TestPerceptron(t *testing.T) {
 	activateNetwork(t, perceptron, []float64{0, 1}, []float64{1})
 	activateNetwork(t, perceptron, []float64{1, 0}, []float64{1})
 	activateNetwork(t, perceptron, []float64{1, 1}, []float64{0})
+}
+
+func TestPerceptronSine(t *testing.T) {
+	perceptron, err := automata.NewPerceptron([]int{1, 12, 1})
+	if err != nil {
+		t.Fatalf("Failed to create NewPerceptron: %s", err.Error())
+	}
+
+	// keep sin output positive
+	sinFunc := func(in float64) float64 {
+		return (math.Sin(in) + 1) / 2
+	}
+
+	// Train it.
+	trainer := automata.Trainer{
+		Network:      perceptron,
+		MaxErrorRate: 1e-6,
+		LearnRate:    0.2,
+		Iterations:   1000,
+		CostFunction: &automata.MeanSquaredErrorCost{},
+	}
+	var ts []automata.TrainSet
+	for i := 0; i < 800; i++ {
+		rads := rand.Float64() * math.Pi * 2 // random radians
+		ts = append(ts, automata.TrainSet{
+			Input:  []float64{rads},
+			Output: []float64{sinFunc(rads)},
+		})
+	}
+	err = trainer.Train(ts)
+	if err != nil {
+		t.Fatalf("trainer.Train threw error: %s", err.Error())
+	}
+
+	// Test it.
+	inputs := []float64{0, 0.5 * math.Pi, 2}
+	outputs := []float64{sinFunc(0), sinFunc(0.5 * math.Pi), sinFunc(2)}
+	for i := 0; i < len(inputs); i++ {
+		output, err := perceptron.Activate([]float64{inputs[i]})
+		if err != nil {
+			t.Errorf("Activate(%v) returned an error: %s", inputs[i], err.Error())
+		}
+		delta := math.Abs(output[0] - outputs[i])
+		if delta > 0.05 {
+			t.Errorf("Activate(%v) returned %v, want %v with delta < 0.05", inputs[i], output[0], outputs[i])
+		}
+	}
 }
