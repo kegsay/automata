fdff4e6cc356ed52b8c54eae441bbc8015e1ef02
Kegsay <kegsay@gmail.com>
2017-10-23 20:26:54 +0100

    Add MSE test (#9)

--- coster_test.go
+++ coster_test.go
@@ -4,6 +4,21 @@ import (
 	"testing"
 )
 
+func TestMeanSquaredErrorCost(t *testing.T) {
+	mse := MeanSquaredErrorCost{}
+	output := mse.Cost([]float64{0, 0.5, 1}, []float64{1, 0.5, 0})
+	want := 0.66666666666666666666
+	if output != want {
+		t.Errorf("Cost([0, 0.5, 1], [1, 0.5, 0]) : want %f, got %f", want, output)
+	}
+
+	output = mse.Cost([]float64{0, 0.5, 1}, []float64{0, 0.5, 1})
+	want = 0
+	if output != want {
+		t.Errorf("Cost([0, 0.5, 1], [0, 0.5, 1]) : want %f, got %f", want, output)
+	}
+}
+
 func TestCrossEntropyCost(t *testing.T) {
 	ce := CrossEntropyCost{}
 	output := ce.Cost([]float64{0, 0.5, 1}, []float64{1, 0.5, 0})
